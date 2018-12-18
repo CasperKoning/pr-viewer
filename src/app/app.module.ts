fc460c8c9a387fb653d1dbf2dc87133f33d19120
CasperKoning <casper.e.koning@gmail.com>
2018-12-18 21:44:02 +0100

    Set up separate PR-Card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PrListComponent } from './pr-list/pr-list.component';
+import { PullRequestCardComponent } from './pull-request-card/pull-request-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrListComponent
+    PrListComponent,
+    PullRequestCardComponent
   ],
   imports: [
     BrowserModule
