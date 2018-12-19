27fe2e9b4adec0b20d1d91cf535f7ee0ae8ead4d
CasperKoning <casper.e.koning@gmail.com>
2018-12-19 21:38:05 +0100

    Model pullrequests as a single accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PrListComponent } from './pr-list/pr-list.component';
-import { PullRequestCardComponent } from './pull-request-card/pull-request-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card'
 import { MatListModule } from '@angular/material/list'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrListComponent,
-    PullRequestCardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule, // Disable with NoopAnimationsModule
     MatCardModule,
     MatListModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
     // Add imports for Material components, like MatButtonModule, MatCheckboxModule, etc.
   ],
   providers: [],
