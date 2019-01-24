f59b24909d1a528189206add8398e662fe202f9e
Casper Koning <casper.e.koning@gmail.com>
2019-01-24 21:19:11 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  githubApiUrl: 'https://my-github-enterprise-url.com/api/graphql',
+  githubApiToken: 'my-github-enterprise-token',
+  supportedOrganizations: ['org1', 'org2', 'org3']
 };
